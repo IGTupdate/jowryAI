a9b82475e8b9a7cf1ae5b4bf07e399dcbc710b02
ramikhreim1 <rami.khreim@symplify.com>
2023-02-03 13:58:49 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,25 @@
+const dev = {
+	baseURL: "http://localhost:3080/api/",
+	landingPageUrl: "http://localhost:3080",
+	stripe: {
+		free: "price_1MX7qPK0vBwAJTX5YXX2Yjio",
+		entry: "price_1MX7n2K0vBwAJTX5oUvhXp0g",
+		pro: "price_1MX7jRK0vBwAJTX5UykbKWxF"
+	},
+};
+
+const prod = {
+	baseURL: '/api/',
+	landingPageUrl: "https://app.openaitemplate.com",
+	stripe: {
+		free: "price_1MX7qPK0vBwAJTX5YXX2Yjio",
+		entry: "price_1MX7n2K0vBwAJTX5oUvhXp0g",
+		pro: "price_1MX7jRK0vBwAJTX5UykbKWxF"
+	},
+};
+
+const config = process.env.NODE_ENV === 'development'
+	? dev
+	: prod;
+
+export default config;
